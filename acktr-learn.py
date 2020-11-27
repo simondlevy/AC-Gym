6acdbc6d841ae9a4b84fbe21f5fcee04521b40d3
simondlevy <simon.d.levy@gmail.com>
2020-11-27 18:07:55 -0500

    More Issue 5 work

--- acktr-learn.py
+++ acktr-learn.py
@@ -12,14 +12,6 @@ import torch
 import torch.optim as optim
 import torch.nn.functional as F
 
-
-REWARD_STEPS = 5
-BATCH_SIZE = 32
-LEARNING_RATE_ACTOR = 1e-3
-LEARNING_RATE_CRITIC = 1e-3
-ENTROPY_BETA = 1e-3
-ENVS_COUNT = 16
-
 def clean_act_net(d):
     # Correct for different key names in ACKTR
     newd = {}
@@ -31,19 +23,26 @@ if __name__ == '__main__':
 
     parser = make_learn_parser()
 
+    parser.add_argument('--reward-steps', default=5, type=int, help='Reward steps')
+    parser.add_argument('--batch-size', default=32, type=int, help='Batch size')
+    parser.add_argument('--lr-actor', default=1e-3, type=float, help='Learning rate for actor')
+    parser.add_argument('--lr-critic', default=1e-3, type=float, help='Learning rate for critic')
+    parser.add_argument('--entropy-beta', default=1e-3, type=float, help='Entropy beta')
+    parser.add_argument('--envs-count', default=16, type=int, help='Environments count')
+
     args, device, save_path, test_env, maxeps, maxsec = parse_args(parser, 'acktr')
 
-    envs = [gym.make(args.env) for _ in range(ENVS_COUNT)]
+    envs = [gym.make(args.env) for _ in range(args.envs_count)]
 
     net_act = model.ModelActor(envs[0].observation_space.shape[0], envs[0].action_space.shape[0], args.nhid).to(device)
     net_crt = model.ModelCritic(envs[0].observation_space.shape[0], args.nhid).to(device)
 
     writer = SummaryWriter(comment='-acktr_' + args.env)
     agent = model.AgentA2C(net_act, device=device)
-    exp_source = ptan.experience.ExperienceSourceFirstLast(envs, agent, args.gamma, steps_count=REWARD_STEPS)
+    exp_source = ptan.experience.ExperienceSourceFirstLast(envs, agent, args.gamma, steps_count=args.reward_steps)
 
-    opt_act = kfac.KFACOptimizer(net_act, lr=LEARNING_RATE_ACTOR)
-    opt_crt = optim.Adam(net_crt.parameters(), lr=LEARNING_RATE_CRITIC)
+    opt_act = kfac.KFACOptimizer(net_act, lr=args.lr_actor)
+    opt_crt = optim.Adam(net_crt.parameters(), lr=args.lr_critic)
 
     batch = []
     best_reward = None
@@ -88,11 +87,11 @@ if __name__ == '__main__':
                         break
 
                 batch.append(exp)
-                if len(batch) < BATCH_SIZE:
+                if len(batch) < args.batch_size:
                     continue
 
                 states_v, actions_v, vals_ref_v = \
-                    common.unpack_batch_a2c(batch, net_crt, last_val_gamma=args.gamma ** REWARD_STEPS, device=device)
+                    common.unpack_batch_a2c(batch, net_crt, last_val_gamma=args.gamma ** args.reward_steps, device=device)
                 batch.clear()
 
                 opt_crt.zero_grad()
@@ -113,7 +112,7 @@ if __name__ == '__main__':
                 opt_act.zero_grad()
                 adv_v = vals_ref_v.unsqueeze(dim=-1) - value_v.detach()
                 loss_policy_v = -(adv_v * log_prob_v).mean()
-                entropy_loss_v = ENTROPY_BETA * (-(torch.log(2*math.pi*torch.exp(net_act.logstd)) + 1)/2).mean()
+                entropy_loss_v = args.entropy_beta * (-(torch.log(2*math.pi*torch.exp(net_act.logstd)) + 1)/2).mean()
                 loss_v = loss_policy_v + entropy_loss_v
                 loss_v.backward()
                 opt_act.step()
