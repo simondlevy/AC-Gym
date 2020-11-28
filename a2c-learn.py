a7fc7e4df4cd759aecb8ca88a1ecbeb2cad8d2e6
simondlevy <simon.d.levy@gmail.com>
2020-11-27 21:45:57 -0500

    Cleanup

--- a2c-learn.py
+++ a2c-learn.py
@@ -39,70 +39,58 @@ if __name__ == '__main__':
 
     with ptan.common.utils.RewardTracker() as tracker:
 
-        with ptan.common.utils.TBMeanTracker() as tb_tracker:
+        for step_idx, exp in enumerate(exp_source):
 
-            for step_idx, exp in enumerate(exp_source):
+            if len(tracker.total_rewards) >= maxeps:
+                break
 
-                if len(tracker.total_rewards) >= maxeps:
-                    break
+            rewards_steps = exp_source.pop_rewards_steps()
 
-                rewards_steps = exp_source.pop_rewards_steps()
+            tcurr = time.time()
 
-                tcurr = time.time()
+            if (tcurr-tstart) >= maxsec:
+                break
+            
+            if rewards_steps:
+                rewards, steps = zip(*rewards_steps)
+                tracker.reward(np.mean(rewards), step_idx)
 
-                if (tcurr-tstart) >= maxsec:
-                    break
-                
-                if rewards_steps:
-                    rewards, steps = zip(*rewards_steps)
-                    tb_tracker.track('episode_steps', np.mean(steps), step_idx)
-                    tracker.reward(np.mean(rewards), step_idx)
-
-                if step_idx % args.test_iters == 0:
-                    reward, steps = test_net(net_act, test_env, device=device)
-                    print('Test done in %.2f sec, reward %.3f, steps %d' % (time.time() - tcurr, reward, steps))
-                    name = '%+.3f_%d.dat' % (reward, step_idx)
-                    fname = save_path + name
-                    if best_reward is None or best_reward < reward:
-                        if best_reward is not None:
-                            print('Best reward updated: %.3f -> %.3f' % (best_reward, reward))
-                            torch.save(net_act.state_dict(), fname)
-                        best_reward = reward
-                    if args.target is not None and reward >= args.target:
-                        print('Target %f achieved; saving %s' % (args.target,fname))
+            if step_idx % args.test_iters == 0:
+                reward, steps = test_net(net_act, test_env, device=device)
+                print('Test done in %.2f sec, reward %.3f, steps %d' % (time.time() - tcurr, reward, steps))
+                name = '%+.3f_%d.dat' % (reward, step_idx)
+                fname = save_path + name
+                if best_reward is None or best_reward < reward:
+                    if best_reward is not None:
+                        print('Best reward updated: %.3f -> %.3f' % (best_reward, reward))
                         torch.save(net_act.state_dict(), fname)
-                        break
+                    best_reward = reward
+                if args.target is not None and reward >= args.target:
+                    print('Target %f achieved; saving %s' % (args.target,fname))
+                    torch.save(net_act.state_dict(), fname)
+                    break
 
-                batch.append(exp)
+            batch.append(exp)
 
-                if len(batch) < args.batch_size:
-                    continue
+            if len(batch) < args.batch_size:
+                continue
 
-                states_v, actions_v, vals_ref_v = \
-                    common.unpack_batch_a2c(batch, net_crt, last_val_gamma=args.gamma ** args.reward_steps, device=device)
-                batch.clear()
+            states_v, actions_v, vals_ref_v = \
+                common.unpack_batch_a2c(batch, net_crt, last_val_gamma=args.gamma ** args.reward_steps, device=device)
+            batch.clear()
 
-                opt_crt.zero_grad()
-                value_v = net_crt(states_v)
-                loss_value_v = F.mse_loss(value_v.squeeze(-1), vals_ref_v)
-                loss_value_v.backward()
-                opt_crt.step()
+            opt_crt.zero_grad()
+            value_v = net_crt(states_v)
+            loss_value_v = F.mse_loss(value_v.squeeze(-1), vals_ref_v)
+            loss_value_v.backward()
+            opt_crt.step()
 
-                opt_act.zero_grad()
-                mu_v = net_act(states_v)
-                adv_v = vals_ref_v.unsqueeze(dim=-1) - value_v.detach()
-                log_prob_v = adv_v * calc_logprob(mu_v, net_act.logstd, actions_v)
-                loss_policy_v = -log_prob_v.mean()
-                entropy_loss_v = args.entropy_beta * (-(torch.log(2*math.pi*torch.exp(net_act.logstd)) + 1)/2).mean()
-                loss_v = loss_policy_v + entropy_loss_v
-                loss_v.backward()
-                opt_act.step()
-
-                tb_tracker.track('advantage', adv_v, step_idx)
-                tb_tracker.track('values', value_v, step_idx)
-                tb_tracker.track('batch_rewards', vals_ref_v, step_idx)
-                tb_tracker.track('loss_entropy', entropy_loss_v, step_idx)
-                tb_tracker.track('loss_policy', loss_policy_v, step_idx)
-                tb_tracker.track('loss_value', loss_value_v, step_idx)
-                tb_tracker.track('loss_total', loss_v, step_idx)
-
+            opt_act.zero_grad()
+            mu_v = net_act(states_v)
+            adv_v = vals_ref_v.unsqueeze(dim=-1) - value_v.detach()
+            log_prob_v = adv_v * calc_logprob(mu_v, net_act.logstd, actions_v)
+            loss_policy_v = -log_prob_v.mean()
+            entropy_loss_v = args.entropy_beta * (-(torch.log(2*math.pi*torch.exp(net_act.logstd)) + 1)/2).mean()
+            loss_v = loss_policy_v + entropy_loss_v
+            loss_v.backward()
+            opt_act.step()
