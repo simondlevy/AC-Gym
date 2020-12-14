f7a069536dbc80d1cd206791993b14ecd4d512e0
simondlevy <simon.d.levy@gmail.com>
2020-12-14 18:11:40 -0500

    Addes setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''
+Python distutils setup file for ac-gym package.
+
+Copyright (C) 2020 Simon D. Levy
+
+MIT License
+'''
+
+#from distutils.core import setup
+from setuptools import setup
+
+setup (name = 'ac_gym',
+    version = '0.1',
+    install_requires = ['gym', 'numpy'],
+    description = 'Use NEAT to learn OpenAI Gym environment',
+    packages = ['ac_gym'],
+    author='Simon D. Levy',
+    author_email='simon.d.levy@gmail.com',
+    url='https://github.com/simondlevy/gym-copter',
+    license='MIT',
+    platforms='Linux; Windows; OS X'
+    )
