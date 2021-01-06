240ff771f517b26652181b06002aa2a63e556c0e
simondlevy <simon.d.levy@gmail.com>
2021-01-06 13:54:40 -0500

    More flakin'

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@ Copyright (C) 2020 Simon D. Levy
 MIT License
 '''
 
-#from distutils.core import setup
 from setuptools import setup
 
-setup (name = 'ac_gym',
-    version = '0.1',
-    install_requires = ['gym', 'numpy'],
-    description = 'Use NEAT to learn OpenAI Gym environment',
-    packages = ['ac_gym', 'ac_gym.ptan', 'ac_gym.ptan.common'],
-    author='Simon D. Levy',
-    author_email='simon.d.levy@gmail.com',
-    url='https://github.com/simondlevy/gym-copter',
-    license='MIT',
-    platforms='Linux; Windows; OS X'
-    )
+setup(
+       name='ac_gym',
+       version='0.1',
+       install_requires=['gym', 'numpy'],
+       description='Use NEAT to learn OpenAI Gym environment',
+       packages=['ac_gym', 'ac_gym.ptan', 'ac_gym.ptan.common'],
+       author='Simon D. Levy',
+       author_email='simon.d.levy@gmail.com',
+       url='https://github.com/simondlevy/gym-copter',
+       license='MIT',
+       platforms='Linux; Windows; OS X'
+      )
