b12f168f34c1e8e483ecab5fa421f8f7332489ce
simondlevy <simon.d.levy@gmail.com>
2021-01-06 15:50:29 -0500

    =More flakin'

--- ac_gym/__init__.py
+++ ac_gym/__init__.py
@@ -6,6 +6,7 @@ import argparse
 import os
 import torch.optim as optim
 
+
 class Solver:
 
     def __init__(self, args, algo_name):
@@ -21,8 +22,11 @@ class Solver:
         self.env_name = args.env
         self.nhid = args.nhid
 
-        self.net_act = model.ModelActor(self.env.observation_space.shape[0], self.env.action_space.shape[0], self.nhid).to(self.device)
-        self.net_crt = model.ModelCritic(self.env.observation_space.shape[0], self.nhid).to(self.device)
+        self.net_act = model.ModelActor(self.env.observation_space.shape[0],
+                                        self.env.action_space.shape[0],
+                                        self.nhid).to(self.device)
+        self.net_crt = model.ModelCritic(self.env.observation_space.shape[0],
+                                         self.nhid).to(self.device)
 
         self.opt_crt = optim.Adam(self.net_crt.parameters(), lr=args.lr_critic)
 
@@ -51,36 +55,48 @@ class Solver:
 
             if episode_idx == maxeps:
                 break
-            
+
             if rewards_steps:
                 rewards, steps = zip(*rewards_steps)
 
             if episode_idx % self.test_iters == 0:
-                reward, steps = test_net(self.net_act, self.env, self.eval_episodes, device=self.device)
-                print('Episode %07d:\treward = %+.3f,\tsteps = %d' % (episode_idx, reward, steps))
+                reward, steps = test_net(self.net_act,
+                                         self.env,
+                                         self.eval_episodes,
+                                         device=self.device)
+                print('Episode %07d:\treward = %+.3f,\tsteps = %d' %
+                      (episode_idx, reward, steps))
                 model_fname = self.models_path + ('%+010.3f.dat' % reward)
                 evaluations.append((episode_idx+1, reward))
-                if self.checkpoint and (best_reward is None or best_reward < reward):
+                if (self.checkpoint and (best_reward is None or
+                                         best_reward < reward)):
                     if best_reward is not None:
-                        print('\n* Best reward updated: %+.3f -> %+.3f *\n' % (best_reward, reward))
+                        print('\n* Best reward updated: %+.3f -> %+.3f *\n' %
+                              (best_reward, reward))
                         self._save(model_fname)
                     best_reward = reward
                 if self.target is not None and reward >= self.target:
-                    print('Target %f achieved; saving %s' % (self.target,model_fname))
+                    print('Target %f achieved; saving %s' %
+                          (self.target, model_fname))
                     self._save(model_fname)
                     break
 
             self.update(exp)
 
-        np.save(self.runs_path+('' if best_reward is None else ('%f'%best_reward)), evaluations)
-        reward, _ = test_net(self.net_act, self.env, self.eval_episodes, device=self.device)
+        np.save(self.runs_path+(''
+                                if best_reward is None
+                                else ('%f' % best_reward)), evaluations)
+        reward, _ = test_net(self.net_act,
+                             self.env,
+                             self.eval_episodes,
+                             device=self.device)
         model_fname = self.models_path + ('%+010.3f.dat' % reward)
         self._save(model_fname)
 
     def _save(self, model_fname):
 
         d = self._clean(self.net_act.state_dict())
-        torch.save((d,self.env_name,self.nhid), open(model_fname, 'wb'))
+        torch.save((d, self.env_name, self.nhid), open(model_fname, 'wb'))
 
     def _clean(self, net):
 
@@ -88,19 +104,28 @@ class Solver:
 
 
 def make_learn_parser():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    fmtr = argparse.ArgumentDefaultsHelpFormatter
+    parser = argparse.ArgumentParser(formatter_class=fmtr)
     parser.add_argument('--env', default='Pendulum-v0', help='Environment id')
-    parser.add_argument('--checkpoint', dest='checkpoint', action='store_true', help='Save at each new best')
+    parser.add_argument('--checkpoint', dest='checkpoint', action='store_true',
+                        help='Save at each new best')
     parser.add_argument('--nhid', default=64, type=int, help='Hidden units')
-    parser.add_argument('--maxeps', default=None, type=int, help='Maximum number of episodes')
-    parser.add_argument('--target', type=float, default=np.inf, help='Quitting criterion for average reward')
+    parser.add_argument('--maxeps', default=None, type=int,
+                        help='Maximum number of episodes')
+    parser.add_argument('--target', type=float, default=np.inf,
+                        help='Quitting criterion for average reward')
     parser.add_argument('--gamma', default=0.99, help='Discount factor')
-    parser.add_argument('--test-iters', default=100, type=float, help='How often to test and save best')
-    parser.add_argument('--eval-episodes', default=10, type=float,  help='How many episodes to evaluate for average')
+    parser.add_argument('--test-iters', default=100, type=float,
+                        help='How often to test and save best')
+    parser.add_argument('--eval-episodes', default=10, type=float,
+                        help='How many episodes to evaluate for average')
 
-    parser.add_argument('--cuda', default=False, action='store_true', help='Enable CUDA')
-    parser.add_argument('--datafile', required=False, help='Name of data file to load')
+    parser.add_argument('--cuda', default=False, action='store_true',
+                        help='Enable CUDA')
+    parser.add_argument('--datafile', required=False,
+                        help='Name of data file to load')
     return parser
+
 
 def test_net(net, env, count=10, device='cpu'):
     rewards = 0.0
@@ -112,7 +137,7 @@ def test_net(net, env, count=10, device='cpu'):
             mu_v = net(obs_v)[0]
             action = mu_v.squeeze(dim=0).data.cpu().numpy()
             action = np.clip(action, -1, 1)
-            if np.isscalar(action): 
+            if np.isscalar(action):
                 action = [action]
             obs, reward, done, _ = env.step(action)
             rewards += reward
@@ -121,9 +146,8 @@ def test_net(net, env, count=10, device='cpu'):
                 break
     return rewards / count, steps / count
 
+
 def calc_logprob(mu_v, logstd_v, actions_v):
     p1 = - ((mu_v - actions_v) ** 2) / (2*torch.exp(logstd_v).clamp(min=1e-3))
     p2 = - torch.log(torch.sqrt(2 * np.pi * torch.exp(logstd_v)))
     return p1 + p2
-
-
--- ac_gym/ptan/ignite.py
+++ ac_gym/ptan/ignite.py
@@ -8,22 +8,24 @@ from ignite.handlers.timing import Timer
 
 
 class EpisodeEvents(enum.Enum):
-    EPISODE_COMPLETED = "episode_completed"
-    BOUND_REWARD_REACHED = "bound_reward_reached"
-    BEST_REWARD_REACHED = "best_reward_reached"
+    EPISODE_COMPLETED = 'episode_completed'
+    BOUND_REWARD_REACHED = 'bound_reward_reached'
+    BEST_REWARD_REACHED = 'best_reward_reached'
 
 
 class EndOfEpisodeHandler:
-    def __init__(self, exp_source: ptan.experience.ExperienceSource, alpha: float = 0.98,
+    def __init__(self,
+                 exp_source: ptan.experience.ExperienceSource,
+                 alpha: float = 0.98,
                  bound_avg_reward: Optional[float] = None,
                  subsample_end_of_episode: Optional[int] = None):
-        """
+        '''
         Construct end-of-episode event handler
         :param exp_source: experience source to use
         :param alpha: smoothing alpha param
         :param bound_avg_reward: optional boundary for average reward
-        :param subsample_end_of_episode: if given, end of episode event will be subsampled by this amount
-        """
+        :param subsample_end_of_episode: if given, end of episode event will
+        be subsampled by this amount '''
         self._exp_source = exp_source
         self._alpha = alpha
         self._bound_avg_reward = bound_avg_reward
@@ -33,21 +35,23 @@ class EndOfEpisodeHandler:
     def attach(self, engine: Engine):
         engine.add_event_handler(Events.ITERATION_COMPLETED, self)
         engine.register_events(*EpisodeEvents)
-        State.event_to_attr[EpisodeEvents.EPISODE_COMPLETED] = "episode"
-        State.event_to_attr[EpisodeEvents.BOUND_REWARD_REACHED] = "episode"
-        State.event_to_attr[EpisodeEvents.BEST_REWARD_REACHED] = "episode"
+        State.event_to_attr[EpisodeEvents.EPISODE_COMPLETED] = 'episode'
+        State.event_to_attr[EpisodeEvents.BOUND_REWARD_REACHED] = 'episode'
+        State.event_to_attr[EpisodeEvents.BEST_REWARD_REACHED] = 'episode'
 
     def __call__(self, engine: Engine):
         for reward, steps in self._exp_source.pop_rewards_steps():
-            engine.state.episode = getattr(engine.state, "episode", 0) + 1
+            engine.state.episode = getattr(engine.state, 'episode', 0) + 1
             engine.state.episode_reward = reward
             engine.state.episode_steps = steps
             engine.state.metrics['reward'] = reward
             engine.state.metrics['steps'] = steps
             self._update_smoothed_metrics(engine, reward, steps)
-            if self._subsample_end_of_episode is None or engine.state.episode % self._subsample_end_of_episode == 0:
+            if (self._subsample_end_of_episode is None or
+               engine.state.episode % self._subsample_end_of_episode == 0):
                 engine.fire_event(EpisodeEvents.EPISODE_COMPLETED)
-            if self._bound_avg_reward is not None and engine.state.metrics['avg_reward'] >= self._bound_avg_reward:
+            if (self._bound_avg_reward is not None and
+               engine.state.metrics['avg_reward'] >= self._bound_avg_reward):
                 engine.fire_event(EpisodeEvents.BOUND_REWARD_REACHED)
             if self._best_avg_reward is None:
                 self._best_avg_reward = engine.state.metrics['avg_reward']
@@ -55,8 +59,12 @@ class EndOfEpisodeHandler:
                 engine.fire_event(EpisodeEvents.BEST_REWARD_REACHED)
                 self._best_avg_reward = engine.state.metrics['avg_reward']
 
-    def _update_smoothed_metrics(self, engine: Engine, reward: float, steps: int):
-        for attr_name, val in zip(('avg_reward', 'avg_steps'), (reward, steps)):
+    def _update_smoothed_metrics(self,
+                                 engine: Engine,
+                                 reward: float,
+                                 steps: int):
+        for attr_name, val in zip(('avg_reward', 'avg_steps'),
+                                  (reward, steps)):
             if attr_name not in engine.state.metrics:
                 engine.state.metrics[attr_name] = val
             else:
@@ -76,15 +84,17 @@ class EpisodeFPSHandler:
         self._fps_smooth_alpha = fps_smooth_alpha
 
     def attach(self, engine: Engine, manual_step: bool = False):
-        self._timer.attach(engine, step=None if manual_step else Events.ITERATION_COMPLETED)
+        self._timer.attach(engine,
+                           step=(None if manual_step
+                                 else Events.ITERATION_COMPLETED))
         engine.add_event_handler(EpisodeEvents.EPISODE_COMPLETED, self)
 
     def step(self):
-        """
-        If manual_step=True on attach(), this method should be used every time we've communicated with environment
-        to get proper FPS
+        '''
+        If manual_step=True on attach(), this method should be used every time
+        we've communicated with environment to get proper FPS
         :return:
-        """
+        '''
         self._timer.step()
 
     def __call__(self, engine: Engine):
@@ -99,23 +109,24 @@ class EpisodeFPSHandler:
                 avg_fps += (1-self._fps_smooth_alpha) * fps
             engine.state.metrics[self.AVG_FPS_METRIC] = avg_fps
             engine.state.metrics[self.FPS_METRIC] = fps
-        engine.state.metrics[self.TIME_PASSED_METRIC] = time.time() - self._started_ts
+        engine.state.metrics[self.TIME_PASSED_METRIC] = (time.time() -
+                                                         self._started_ts)
         self._timer.reset()
 
 
 class PeriodEvents(enum.Enum):
-    ITERS_10_COMPLETED = "iterations_10_completed"
-    ITERS_100_COMPLETED = "iterations_100_completed"
-    ITERS_1000_COMPLETED = "iterations_1000_completed"
-    ITERS_10000_COMPLETED = "iterations_10000_completed"
-    ITERS_100000_COMPLETED = "iterations_100000_completed"
+    ITERS_10_COMPLETED = 'iterations_10_completed'
+    ITERS_100_COMPLETED = 'iterations_100_completed'
+    ITERS_1000_COMPLETED = 'iterations_1000_completed'
+    ITERS_10000_COMPLETED = 'iterations_10000_completed'
+    ITERS_100000_COMPLETED = 'iterations_100000_completed'
 
 
 class PeriodicEvents:
-    """
-    The same as CustomPeriodicEvent from ignite.contrib, but use true amount of iterations,
-    which is good for TensorBoard
-    """
+    '''
+    The same as CustomPeriodicEvent from ignite.contrib, but use true amount
+    of iterations, which is good for TensorBoard
+    '''
 
     INTERVAL_TO_EVENT = {
         10: PeriodEvents.ITERS_10_COMPLETED,
@@ -129,10 +140,9 @@ class PeriodicEvents:
         engine.add_event_handler(Events.ITERATION_COMPLETED, self)
         engine.register_events(*PeriodEvents)
         for e in PeriodEvents:
-            State.event_to_attr[e] = "iteration"
+            State.event_to_attr[e] = 'iteration'
 
     def __call__(self, engine: Engine):
         for period, event in self.INTERVAL_TO_EVENT.items():
             if engine.state.iteration % period == 0:
                 engine.fire_event(event)
-
