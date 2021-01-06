cbeceb4f3ed2781ca82014d12fdfba123a3b5ed3
simondlevy <simon.d.levy@gmail.com>
2021-01-06 16:42:38 -0500

    =More flakin'

--- ac_gym/ptan/common/wrappers.py
+++ ac_gym/ptan/common/wrappers.py
@@ -1,17 +1,16 @@
-"""basic wrappers, useful for reinforcement learning on gym envs"""
-# Mostly copy-pasted from https://github.com/openai/baselines/blob/master/baselines/common/atari_wrappers.py
+'''basic wrappers, useful for reinforcement learning on gym envs'''
+# github.com/openai/baselines/blob/master/baselines/common/atari_wrappers.py
 import numpy as np
 from collections import deque
 import gym
 from gym import spaces
-#import cv2 # XXX do we need cv2 (OpenCV)?
 
 
 class NoopResetEnv(gym.Wrapper):
     def __init__(self, env=None, noop_max=30):
-        """Sample initial states by taking random number of no-ops on reset.
+        '''Sample initial states by taking random number of no-ops on reset.
         No-op is assumed to be action 0.
-        """
+        '''
         super(NoopResetEnv, self).__init__(env)
         self.noop_max = noop_max
         self.override_num_noops = None
@@ -21,7 +20,7 @@ class NoopResetEnv(gym.Wrapper):
         return self.env.step(action)
 
     def reset(self):
-        """ Do no-op action for a number of steps in [1, noop_max]."""
+        ''' Do no-op action for a number of steps in [1, noop_max].'''
         self.env.reset()
         if self.override_num_noops is not None:
             noops = self.override_num_noops
@@ -38,7 +37,10 @@ class NoopResetEnv(gym.Wrapper):
 
 class FireResetEnv(gym.Wrapper):
     def __init__(self, env=None):
-        """For environments where the user need to press FIRE for the game to start."""
+        '''
+        For environments where the user need to press FIRE for the game to
+        start.
+        '''
         super(FireResetEnv, self).__init__(env)
         assert env.unwrapped.get_action_meanings()[1] == 'FIRE'
         assert len(env.unwrapped.get_action_meanings()) >= 3
@@ -59,9 +61,11 @@ class FireResetEnv(gym.Wrapper):
 
 class EpisodicLifeEnv(gym.Wrapper):
     def __init__(self, env=None):
-        """Make end-of-life == end-of-episode, but only reset on true game over.
-        Done by DeepMind for the DQN and co. since it helps value estimation.
-        """
+        '''
+        Make end-of-life == end-of-episode, but only reset on true game
+        over.  Done by DeepMind for the DQN and co. since it helps value
+        estimation.
+        '''
         super(EpisodicLifeEnv, self).__init__(env)
         self.lives = 0
         self.was_real_done = True
@@ -74,18 +78,19 @@ class EpisodicLifeEnv(gym.Wrapper):
         # then update lives to handle bonus lives
         lives = self.env.unwrapped.ale.lives()
         if lives < self.lives and lives > 0:
-            # for Qbert somtimes we stay in lives == 0 condtion for a few frames
-            # so its important to keep lives > 0, so that we only reset once
-            # the environment advertises done.
+            # for Qbert somtimes we stay in lives == 0 condtion for a few
+            # frames so its important to keep lives > 0, so that we only reset
+            # once the environment advertises done.
             done = True
         self.lives = lives
         return obs, reward, done, info
 
     def reset(self):
-        """Reset only when lives are exhausted.
-        This way all states are still reachable even though lives are episodic,
-        and the learner need not know about any of this behind-the-scenes.
-        """
+        '''
+        Reset only when lives are exhausted.  This way all states are still
+        reachable even though lives are episodic, and the learner need not
+        know about any of this behind-the-scenes.
+        '''
         if self.was_real_done:
             obs = self.env.reset()
             self.was_real_reset = True
@@ -99,7 +104,7 @@ class EpisodicLifeEnv(gym.Wrapper):
 
 class MaxAndSkipEnv(gym.Wrapper):
     def __init__(self, env=None, skip=4):
-        """Return only every `skip`-th frame"""
+        '''Return only every `skip`-th frame'''
         super(MaxAndSkipEnv, self).__init__(env)
         # most recent raw observations (for max pooling across time steps)
         self._obs_buffer = deque(maxlen=2)
@@ -120,7 +125,7 @@ class MaxAndSkipEnv(gym.Wrapper):
         return max_frame, total_reward, done, info
 
     def reset(self):
-        """Clear past frame buffer and init. to first obs. from inner env."""
+        '''Clear past frame buffer and init. to first obs. from inner env.'''
         self._obs_buffer.clear()
         obs = self.env.reset()
         self._obs_buffer.append(obs)
@@ -130,39 +135,55 @@ class MaxAndSkipEnv(gym.Wrapper):
 class ProcessFrame84(gym.ObservationWrapper):
     def __init__(self, env=None):
         super(ProcessFrame84, self).__init__(env)
-        self.observation_space = spaces.Box(low=0, high=255, shape=(84, 84, 1), dtype=np.uint8)
+        self.observation_space = spaces.Box(low=0,
+                                            high=255,
+                                            shape=(84, 84, 1),
+                                            dtype=np.uint8)
 
     def observation(self, obs):
         return ProcessFrame84.process(obs)
 
     @staticmethod
     def process(frame):
+        '''
         if frame.size == 210 * 160 * 3:
             img = np.reshape(frame, [210, 160, 3]).astype(np.float32)
         elif frame.size == 250 * 160 * 3:
             img = np.reshape(frame, [250, 160, 3]).astype(np.float32)
         else:
-            assert False, "Unknown resolution."
-        img = img[:, :, 0] * 0.299 + img[:, :, 1] * 0.587 + img[:, :, 2] * 0.114
-        #resized_screen = cv2.resize(img, (84, 110), interpolation=cv2.INTER_AREA)
+            assert False, 'Unknown resolution.'
+        img = (img[:, :, 0] * 0.299 +
+               img[:, :, 1] * 0.587 +
+               img[:, :, 2] * 0.114)
+        resized_screen = cv2.resize(img,
+                                    (84, 110),
+                                    interpolation=cv2.INTER_AREA)
+
         x_t = resized_screen[18:102, :]
         x_t = np.reshape(x_t, [84, 84, 1])
         return x_t.astype(np.uint8)
+        '''
+        return 0
 
 
 class ClippedRewardsWrapper(gym.RewardWrapper):
     def reward(self, reward):
-        """Change all the positive rewards to 1, negative to -1 and keep zero."""
+        '''
+        Change all the positive rewards to 1, negative to -1 and keep
+        zero.
+        '''
         return np.sign(reward)
 
 
 class LazyFrames(object):
     def __init__(self, frames):
-        """This object ensures that common frames between the observations are only stored once.
-        It exists purely to optimize memory usage which can be huge for DQN's 1M frames replay
-        buffers.
-        This object should only be converted to numpy array before being passed to the model.
-        You'd not belive how complex the previous solution was."""
+        '''
+        This object ensures that common frames between the observations are
+        only stored once.  It exists purely to optimize memory usage which can
+        be huge for DQN's 1M frames replay buffers.  This object should only
+        be converted to numpy array before being passed to the model.
+        You'd not belive how complex the previous solution was.
+        '''
         self._frames = frames
 
     def __array__(self, dtype=None):
@@ -174,17 +195,22 @@ class LazyFrames(object):
 
 class FrameStack(gym.Wrapper):
     def __init__(self, env, k):
-        """Stack k last frames.
+        '''Stack k last frames.
         Returns lazy array, which is much more memory efficient.
         See Also
         --------
         baselines.common.atari_wrappers.LazyFrames
-        """
+        '''
         gym.Wrapper.__init__(self, env)
         self.k = k
         self.frames = deque([], maxlen=k)
         shp = env.observation_space.shape
-        self.observation_space = spaces.Box(low=0, high=255, shape=(shp[0]*k, shp[1], shp[2]), dtype=np.float32)
+        self.observation_space = spaces.Box(low=0,
+                                            high=255,
+                                            shape=(shp[0]*k,
+                                                   shp[1],
+                                                   shp[2]),
+                                            dtype=np.float32)
 
     def reset(self):
         ob = self.env.reset()
@@ -210,13 +236,17 @@ class ScaledFloatFrame(gym.ObservationWrapper):
 
 
 class ImageToPyTorch(gym.ObservationWrapper):
-    """
+    '''
     Change image shape to CWH
-    """
+    '''
     def __init__(self, env):
         super(ImageToPyTorch, self).__init__(env)
         old_shape = self.observation_space.shape
-        self.observation_space = gym.spaces.Box(low=0.0, high=1.0, shape=(old_shape[-1], old_shape[0], old_shape[1]),
+        self.observation_space = gym.spaces.Box(low=0.0,
+                                                high=1.0,
+                                                shape=(old_shape[-1],
+                                                       old_shape[0],
+                                                       old_shape[1]),
                                                 dtype=np.float32)
 
     def observation(self, observation):
@@ -224,7 +254,7 @@ class ImageToPyTorch(gym.ObservationWrapper):
 
 
 def wrap_dqn(env, stack_frames=4, episodic_life=True, reward_clipping=True):
-    """Apply a common set of wrappers for Atari games."""
+    '''Apply a common set of wrappers for Atari games.'''
     assert 'NoFrameskip' in env.spec.id
     if episodic_life:
         env = EpisodicLifeEnv(env)
