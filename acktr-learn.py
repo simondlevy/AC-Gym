21b051936a42ddf0266f43c5afa6560399673e30
simondlevy <simon.d.levy@gmail.com>
2021-01-06 14:52:44 -0500

    More flakin'

--- acktr-learn.py
+++ acktr-learn.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import math
 
-from ac_gym import Solver, ptan, model, common, kfac, calc_logprob, make_learn_parser
+from ac_gym import Solver, model, common, kfac, calc_logprob, make_learn_parser
+from ac_gym.ptan.experience import ExperienceSourceFirstLast
 
 import gym
 import torch
 import torch.nn.functional as F
+
 
 class ACKTR(Solver):
 
@@ -17,11 +19,15 @@ class ACKTR(Solver):
 
         agent = model.AgentA2C(self.net_act, device=self.device)
 
-        self. exp_source = ptan.experience.ExperienceSourceFirstLast(envs, agent, args.gamma, steps_count=args.reward_steps)
+        steps = args.reward_steps
+        self. exp_source = ExperienceSourceFirstLast(envs,
+                                                     agent,
+                                                     args.gamma,
+                                                     steps_count=steps)
 
         self.opt_act = kfac.KFACOptimizer(self.net_act, lr=args.lr_actor)
 
-        self.batch_size   = args.batch_size
+        self.batch_size = args.batch_size
         self.reward_steps = args.reward_steps
         self.entropy_beta = args.entropy_beta
 
@@ -31,9 +37,13 @@ class ACKTR(Solver):
         if len(self.batch) < self.batch_size:
             return
 
-        states_v, actions_v, vals_ref_v = \
-            common.unpack_batch_a2c(self.batch, self.net_crt, 
-                    last_val_gamma=self.gamma ** self.reward_steps, device=self.device)
+        g = self.gamma ** self.reward_steps
+
+        states_v, actions_v, vals_ref_v = (
+            common.unpack_batch_a2c(self.batch,
+                                    self.net_crt,
+                                    last_val_gamma=g,
+                                    device=self.device))
         self.batch.clear()
 
         self.opt_crt.zero_grad()
@@ -54,7 +64,10 @@ class ACKTR(Solver):
         self.opt_act.zero_grad()
         adv_v = vals_ref_v.unsqueeze(dim=-1) - value_v.detach()
         loss_policy_v = -(adv_v * log_prob_v).mean()
-        entropy_loss_v = self.entropy_beta * (-(torch.log(2*math.pi*torch.exp(self.net_act.logstd)) + 1)/2).mean()
+        entropy_loss_v = (self.entropy_beta *
+                          (-(torch.log(2*math.pi *
+                                       torch.exp(self.net_act.logstd))
+                              + 1)/2).mean())
         loss_v = loss_policy_v + entropy_loss_v
         loss_v.backward()
         self.opt_act.step()
@@ -64,25 +77,33 @@ class ACKTR(Solver):
         # Correct for different key names in ACKTR
         new = {}
         for oldkey in old.keys():
-            newkey = oldkey.replace('.module','').replace('add_bias._', '')
+            newkey = oldkey.replace('.module', '').replace('add_bias._', '')
             new[newkey] = old[oldkey]
             if 'bias' in newkey:
                 new[newkey] = new[newkey].flatten()
         return new
 
+
 def main():
 
     parser = make_learn_parser()
 
-    parser.add_argument('--reward-steps', default=5, type=int, help='Reward steps')
-    parser.add_argument('--batch-size', default=32, type=int, help='Batch size')
-    parser.add_argument('--lr-actor', default=1e-3, type=float, help='Learning rate for actor')
-    parser.add_argument('--lr-critic', default=1e-3, type=float, help='Learning rate for critic')
-    parser.add_argument('--entropy-beta', default=1e-3, type=float, help='Entropy beta')
-    parser.add_argument('--envs-count', default=16, type=int, help='Environments count')
+    parser.add_argument('--reward-steps', default=5, type=int,
+                        help='Reward steps')
+    parser.add_argument('--batch-size', default=32, type=int,
+                        help='Batch size')
+    parser.add_argument('--lr-actor', default=1e-3, type=float,
+                        help='Learning rate for actor')
+    parser.add_argument('--lr-critic', default=1e-3, type=float,
+                        help='Learning rate for critic')
+    parser.add_argument('--entropy-beta', default=1e-3, type=float,
+                        help='Entropy beta')
+    parser.add_argument('--envs-count', default=16, type=int,
+                        help='Environments count')
 
     args = parser.parse_args()
 
     ACKTR(args).loop()
+
 
 main()
