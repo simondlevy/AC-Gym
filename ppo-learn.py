4f0a2984f52656d16148cd4f2ac2106e818a69e5
simondlevy <simon.d.levy@gmail.com>
2021-01-06 14:43:36 -0500

    More flakin'

--- ppo-learn.py
+++ ppo-learn.py
@@ -7,6 +7,7 @@ import torch
 import torch.optim as optim
 import torch.nn.functional as F
 
+
 class PPO(Solver):
 
     def __init__(self, args):
@@ -17,16 +18,18 @@ class PPO(Solver):
 
         agent = model.AgentA2C(self.net_act, device=self.device)
 
-        self.exp_source = ptan.experience.ExperienceSource(env, agent, steps_count=1)
+        self.exp_source = ptan.experience.ExperienceSource(env,
+                                                           agent,
+                                                           steps_count=1)
 
         self.opt_act = optim.Adam(self.net_act.parameters(), lr=args.lr_actor)
 
         self.trajectory = []
         self.batch_size = args.batch_size
-        self.traj_size  = args.traj_size
+        self.traj_size = args.traj_size
         self.gae_lambda = args.gae_lambda
-        self.epochs     = args.epochs
-        self.epsilon    = args.epsilon
+        self.epochs = args.epochs
+        self.epsilon = args.epsilon
 
     def update(self, exp):
 
@@ -42,13 +45,16 @@ class PPO(Solver):
         traj_actions_v = traj_actions_v.to(self.device)
         traj_adv_v, traj_ref_v = self.calc_adv_ref(traj_states_v)
         mu_v = self.net_act(traj_states_v)
-        old_logprob_v = calc_logprob( mu_v, self.net_act.logstd, traj_actions_v)
+        old_logprob_v = calc_logprob(mu_v,
+                                     self.net_act.logstd,
+                                     traj_actions_v)
 
         # normalize advantages
         traj_adv_v = traj_adv_v - torch.mean(traj_adv_v)
         traj_adv_v /= torch.std(traj_adv_v)
 
-        # drop last entry from the trajectory, an our adv and ref value calculated without it
+        # drop last entry from the trajectory, an our adv and ref value
+        # calculated without it
         self.trajectory = self.trajectory[:-1]
         old_logprob_v = old_logprob_v[:-1].detach()
 
@@ -85,7 +91,8 @@ class PPO(Solver):
                 ratio_v = torch.exp(
                     logprob_pi_v - batch_old_logprob_v)
                 surr_obj_v = batch_adv_v * ratio_v
-                c_ratio_v = torch.clamp(ratio_v, 1.0 - self.epsilon, 1.0 + self.epsilon)
+                c_ratio_v = torch.clamp(ratio_v, 1.0 - self.epsilon,
+                                        1.0 + self.epsilon)
                 clipped_surr_v = batch_adv_v * c_ratio_v
                 loss_policy_v = -torch.min(
                     surr_obj_v, clipped_surr_v).mean()
@@ -137,16 +144,22 @@ def main():
 
     parser = make_learn_parser()
 
-    parser.add_argument('--gae-lambda', default=0.95, type=float, help='Lambda for Generalized Advantage Estimation')
-    parser.add_argument('--traj-size', default=2049, type=int, help='Trajectory size')
-    parser.add_argument('--lr-actor', default=1e-5, type=float, help='Learning rate for actor')
-    parser.add_argument('--lr-critic', default=1e-4, type=float, help='Learning rate for critic')
+    parser.add_argument('--gae-lambda', default=0.95, type=float,
+                        help='Lambda for Generalized Advantage Estimation')
+    parser.add_argument('--traj-size', default=2049, type=int,
+                        help='Trajectory size')
+    parser.add_argument('--lr-actor', default=1e-5, type=float,
+                        help='Learning rate for actor')
+    parser.add_argument('--lr-critic', default=1e-4, type=float,
+                        help='Learning rate for critic')
     parser.add_argument('--epsilon', default=0.2, type=float, help='Clipping')
     parser.add_argument('--epochs', default=10, type=int, help='Epochs')
-    parser.add_argument('--batch-size', default=64, type=int, help='Batch size')
+    parser.add_argument('--batch-size', default=64, type=int,
+                        help='Batch size')
 
     args = parser.parse_args()
 
     PPO(args).loop()
+
 
 main()
