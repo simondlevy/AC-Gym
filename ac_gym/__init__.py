37e45ffb6edc0990339845722bab7210388b084d
simondlevy <simon.d.levy@gmail.com>
2021-02-12 14:30:57 -0500

    Cleanup

--- ac_gym/__init__.py
+++ ac_gym/__init__.py
@@ -8,13 +8,6 @@ from time import time
 import torch.optim as optim
 
 
-def save_history(pathname, history):
-    with open(pathname + '.csv', 'w') as csvfile:
-        csvfile.write('Iter,Time,Reward\n')
-        for row in history[:-1]:  # last reward is always zero
-            csvfile.write('%d,%f,%f\n' % (row[0], row[1], row[2]))
-
-
 class Solver:
 
     def __init__(self, args, algo_name):
@@ -83,14 +76,14 @@ class Solver:
                     if best_reward is not None:
                         print('\n* Best reward updated: %+.3f -> %+.3f *\n' %
                               (best_reward, reward))
-                        self._save(model_fname)
+                        self._save_model(model_fname)
                         csvfilename = self.runs_path+('%f' % best_reward)
-                        save_history(csvfilename, history)
+                        self._save_history(csvfilename, history)
                     best_reward = reward
                 if self.target is not None and reward >= self.target:
                     print('Target %f achieved; saving %s' %
                           (self.target, model_fname))
-                    self._save(model_fname)
+                    self._save_model(model_fname)
                     break
 
             self.update(exp)
@@ -99,19 +92,25 @@ class Solver:
                                       if best_reward is None
                                       else ('%f' % best_reward))
 
-        save_history(csvfilename, history)
+        self._save_history(csvfilename, history)
 
         reward, _ = test_net(self.net_act,
                              self.env,
                              self.eval_episodes,
                              device=self.device)
         model_fname = self.models_path + ('%+010.3f.dat' % reward)
-        self._save(model_fname)
+        self._save_model(model_fname)
 
-    def _save(self, model_fname):
+    def _save_model(self, model_fname):
 
         d = self._clean(self.net_act.state_dict())
         torch.save((d, self.env_name, self.nhid), open(model_fname, 'wb'))
+
+    def _save_history(self, pathname, history):
+        with open(pathname + '.csv', 'w') as csvfile:
+            csvfile.write('Iter,Time,Reward\n')
+            for row in history:  # last reward is always zero
+                csvfile.write('%d,%f,%f\n' % (row[0], row[1], row[2]))
 
     def _clean(self, net):
 
